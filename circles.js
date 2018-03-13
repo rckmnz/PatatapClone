5ca0d4f55f2c4c5aa7b22d048176d55fc97dead6
rckmnz <rckmnz05@gmail.com>
2018-03-13 14:40:15 -0400

    trying to fix it

--- circles.js
+++ circles.js
@@ -1,4 +1,4 @@
-var keyData = {
+let keyData = {
     q: {
         sound: new Howl({
             src: ['sounds/bubbles.mp3']
@@ -158,27 +158,30 @@ var keyData = {
 }
 
 
-var circles = [];
+let circles = [];
 
 function onKeyDown(event) {
     if (keyData[event.key]) {
-
-
         var maxPoint = new Point(view.size.width, view.size.height);
         var randomPoint = Point.random();
         var point = maxPoint * randomPoint;
-
-        var newCircle = new Path.Circle(point, 500);
+        var newCircle = new Path.Circle(point, 500)
         newCircle.fillColor = keyData[event.key].color;
-        keyData[event.key].sound.play();
+        keyData[event.key]
+            .sound
+            .play();
         circles.push(newCircle);
     }
 }
 
 function onFrame(event) {
     for (var i = 0; i < circles.length; i++) {
+        circles[i].scale(0.9);
         circles[i].fillColor.hue += 1;
-        circles[i].scale(.9);
+        if (circles[i].area < 1) {
+            circles[i].remove();
+            circles.splice(i, 1);
+            console.log(circles);
+        }
     }
-
 }
